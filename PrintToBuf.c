d447d7bd0bb52d6030f29d19f04c92bbb24ba8ea
trteodor <trteodor@gmail.com>
2021-06-18 23:42:43 +0200

    comments

--- PrintToBuf.c
+++ PrintToBuf.c
@@ -2,20 +2,8 @@
 #include "USART_UART_DMA.h"
 
 extern UART_DMA_Handle_Td TUART2;
-char TableBuf[2000];
-int TablePointer=0;
+
 volatile uint32_t shortblocke=0;
-
-void PrintfToBuf(char *String)
-{
-	
-	while(*String++)
-	{
-		TableBuf[TablePointer]=*String;
-		TablePointer++;
-	}
-}
-
 
 void _putchar(char character)
 {
